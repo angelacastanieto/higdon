57f5a238f46aa7f1e3c99db14fc4b852e202b997
angelacastanieto <angela@gametime.co>
2018-03-21 23:36:12 -0700

    fix headers and such for csv

--- higdon.rb
+++ higdon.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 require 'csv'
 
 OUTPUT_HEADER = ["Subject", "Start Date", "All Day Event", "Start Time", "End Time", "Location", "Description"]
+WEEK_HEADER = ["Week starting", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
 
 ALL_DAY_EVENT = true
 START_TIME = ""
@@ -44,12 +45,15 @@ get '/novice-1/week' do
 
   training_date = start_date
 
-  doc.xpath('//table/tbody/tr').each do |row|
+  doc.xpath('//table/tbody/tr').each_with_index do |row, i|
+    next if i == 0 # skip header
+
     tarray = []
 
     tarray << training_date.strftime("%m/%d/%Y")
 
-    row.xpath('td').each do |cell|
+    row.xpath('td').each_with_index do |cell, j|
+      next if j == 0 # skip week numberss
       tarray << cell.text
     end
 
@@ -59,6 +63,46 @@ get '/novice-1/week' do
   end
 
   erb :index, :locals => {:rows => rows, :table_title => 'Marathon Novice 1', :racedate => params['racedate']}
+end
+
+get '/novice-1/week/csv' do
+  race_date = Date.strptime(params['racedate'], '%Y-%m-%d')
+
+  rows = CSV.read("tmp/full-novice-1.csv")
+
+  num_training_days = (rows.count - 1) * 7 - 1 # subtract 1 for header, mult by num days in week, off by one for some reason
+
+  start_date = race_date - num_training_days
+
+  doc = Nokogiri::HTML(open("http://www.halhigdon.com/training/51137/Marathon-Novice-1-Training-Program"))
+
+  rows = []
+
+  training_date = start_date
+
+  csv = CSV.open("tmp/full-novice-1-week.csv", 'w',{:col_sep => ",", :quote_char => '\'', :force_quotes => true})
+
+  csv << WEEK_HEADER
+  doc.xpath('//table/tbody/tr').each_with_index do |row, i|
+    next if i == 0 # skip header
+
+    tarray = []
+
+    tarray << training_date.strftime("%m/%d/%Y")
+
+    row.xpath('td').each_with_index do |cell, j|
+      next if j == 0 # skip week numberss
+      tarray << cell.text
+    end
+
+    training_date += 7 unless tarray.empty? # if is empty, was header row so don't increment training_date
+
+    csv << tarray
+  end
+
+  csv.close
+
+  send_file File.join('tmp/full-novice-1-week.csv'), :filename => 'marathon-novice-1-week', :type => 'Application/octet-stream'
 end
 
 get '/novice-1/csv' do
